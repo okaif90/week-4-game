243bf838855f243065a54868316cf783245d61ac
Omar Kaif <Omar Kaif>
2017-05-07 20:23:11 -0700

    Version 2

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+//Global Variables
+var computerNumber = 0;
+var totalScore = 0;
+var winCount = 0;
+var loseCount = 0;
+var savedNum1 = Math.floor(Math.random() * 12) + 1;
+var savedNum2 = Math.floor(Math.random() * 12) + 1;
+var savedNum3 = Math.floor(Math.random() * 12) + 1;
+var savedNum4 = Math.floor(Math.random() * 12) + 1;
+
+//Click Functions
+
+$("#cartman").click(function() {
+  totalScore=totalScore+savedNum1;    
+  $("#scoreNum").html(totalScore);
+  scoreCheck();
+});
+
+$("#kenny").click(function() {
+  totalScore=totalScore+savedNum2;    
+  $("#scoreNum").html(totalScore);
+  scoreCheck();
+});
+
+
+$("#stan").click(function() {
+  totalScore=totalScore+savedNum3;    
+  $("#scoreNum").html(totalScore);
+  scoreCheck();
+});
+
+
+$("#kyle").click(function() {
+  totalScore=totalScore+savedNum4;
+  $("#scoreNum").html(totalScore);
+  scoreCheck();
+});
+
+//Main Process
+newGame();
+
+function newGame() {
+totalScore = 0; 
+computerNumber =  Math.floor(Math.random() * (102) + 19);
+savedNum1 = Math.floor(Math.random() * 12) + 1;
+savedNum2 = Math.floor(Math.random() * 12) + 1;
+savedNum3 = Math.floor(Math.random() * 12) + 1;
+savedNum4 = Math.floor(Math.random() * 12) + 1;
+$("#compNum").html(computerNumber);
+$("#scoreNum").html(totalScore);
+};
+
+
+function scoreCheck() {
+  if (totalScore==computerNumber){
+    winCount++;
+    $("#winCount").html(winCount);
+    $("#message").html("You won!");
+    newGame();
+  }else if(totalScore>computerNumber){
+    loseCount++;
+    $("#loseCount").html(loseCount);
+    $("#message").html("You lost");
+    newGame();
+  }
+}
+  
+});
